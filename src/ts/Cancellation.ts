6780127b990d78e948078eabb624656c4442df93
cin <cin@localhost>
2018-09-11 01:23:22 +0300

    working on core/Cancellation

--- src/ts/Cancellation.ts
+++ src/ts/Cancellation.ts
@@ -1,33 +1,67 @@
 import { ICancellation } from "./interfaces";
+import { argumentNotNull } from "./safe";
 
 export class Cancellation implements ICancellation {
+    private _reason: any;
+    private _cbs: Array<(e) => void>;
+
+    constructor(action: (cancel: (e) => void) => void) {
+        argumentNotNull(action, "action");
+
+        action(this._cancel.bind(this));
+    }
+
     isSupported(): boolean {
-        return false;
+        return true;
     }
     throwIfRequested(): void {
+        if (this._reason)
+            throw this._reason;
     }
-    
+
     isRequested(): boolean {
-        return false;
+        return !!this._reason;
     }
 
-    register(_cb: (e:any) => void): void {
-        
+    register(cb: (e: any) => void): void {
+        argumentNotNull(cb, "cb");
+
+        if (this._reason) {
+            cb(this._reason);
+        } else {
+            if (!this._cbs)
+                this._cbs = [cb];
+            else
+                this._cbs.push(cb);
+        }
     }
 
-    static readonly none : Cancellation = {
+    private _cancel(reason) {
+        if (this._reason)
+            return;
+
+        this._reason = (reason = reason || new Error("Operation cancelled"));
+
+
+        if (this._cbs) {
+            this._cbs.forEach(cb => cb(reason));
+            this._cbs = null;
+        }
+    }
+
+    static readonly none: ICancellation = {
         isSupported(): boolean {
             return false;
         },
 
         throwIfRequested(): void {
         },
-        
+
         isRequested(): boolean {
             return false;
         },
-    
-        register(_cb: (e:any) => void): void {
+
+        register(_cb: (e: any) => void): void {
         }
     };
 }
