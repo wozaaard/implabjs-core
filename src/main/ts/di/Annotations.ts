dafe4c0d3b979541afb18501283dc745adad34ba
cin <cin@localhost>
2020-06-19 21:56:45 +0300

    dependency builder proposal

--- src/main/ts/di/Annotations.ts
+++ src/main/ts/di/Annotations.ts
@@ -4,7 +4,33 @@ export interface InjectOptions {
     lazy?: boolean;
 }
 
-export function inject<I extends { [name in keyof I]: (v: any) => void } >(dependency: string) {
+type Setter<T = any> = (v: T) => void;
+
+type Injector<T> = {
+    [k in keyof T]: Setter;
+};
+
+export class Builder<T, S> {
+    provides(name: string) {
+        return <C extends Constructor<T>>(constructor: C) => {
+            return constructor;
+        };
+    }
+
+    inject<K extends keyof S>(dependency: K) {
+        return <M extends keyof T>(target: any, memberName: M, descriptor: TypedPropertyDescriptor<Setter<S[K]>> ) => {
+
+        };
+    }
+
+    prop<K extends keyof S>(dependency: K) {
+        return <M extends keyof T>(target: any, memberName: M, descriptor: TypedPropertyDescriptor<S[K]> ) => {
+
+        };
+    }
+}
+
+export function inject<I extends Injector<I> >(dependency: string) {
     return <M extends keyof I>(target: any, memberName: M, descriptor: TypedPropertyDescriptor<I[M]> ) => {
 
     };
--- src/test/ts/mock/Box.ts
+++ src/test/ts/mock/Box.ts
@@ -1,13 +1,18 @@
-import { inject } from "../di/Annotations";
+import { Builder } from "../di/Annotations";
+import { Bar } from "./Bar";
+import { Foo } from "./Foo";
 
 export interface Injector<T> {
     setValue(value: T);
 }
 
+const builder = new Builder<Box<any>, { bar: Bar; foo: Foo}>();
+
+@builder.provides("barBox")
 export class Box<T> implements Injector<T> {
     private _value: T;
 
-    @inject<Injector<T>>("bar")
+    @builder.inject("bar")
     setValue(value: T) {
         this._value = value;
     }
