a8d1ebba6340c553c5b0c14a94a0a6cee1db1958
cin <cin@localhost>
2020-06-22 09:59:48 +0300

    working on di decorators

--- src/test/ts/mock/Bar.ts
+++ src/test/ts/mock/Bar.ts
@@ -3,10 +3,16 @@ import { Foo } from "./Foo";
 export class Bar {
     name = "bar";
 
-    foo: Foo;
+    foo: Foo | undefined;
 
-    constructor(_opts) {
+    constructor(_opts?: { foo?: Foo; }) {
         if (_opts && _opts.foo)
             this.foo = _opts.foo;
+    }
+
+    getFoo() {
+        if (this.foo === undefined)
+            throw new Error("The foo isn't set");
+        return this.foo;
     }
 }
--- src/test/ts/mock/Box.ts
+++ src/test/ts/mock/Box.ts
@@ -2,22 +2,32 @@ import { Builder } from "../di/Annotations";
 import { Bar } from "./Bar";
 import { Foo } from "./Foo";
 
-export interface Injector<T> {
-    setValue(value: T);
-}
+const builder = new Builder<Box<Bar>, { bar: Bar; foo: Foo; obj: object }>();
 
-const builder = new Builder<Box<any>, { bar: Bar; foo: Foo}>();
+@builder.provides()
+@builder.dependencies("bar")
+export class Box<T> {
+    private _value: T | undefined;
 
-@builder.provides("barBox")
-export class Box<T> implements Injector<T> {
-    private _value: T;
+    constructor(value: T) {
+        this._value = value;
+    }
 
     @builder.inject("bar")
     setValue(value: T) {
         this._value = value;
     }
 
+
+    @builder.inject("foo")
+    setObj(value: object) {
+
+    }
+
     getValue() {
+        if (this._value === undefined)
+            throw new Error("Trying to get a value from the empty box");
+
         return this._value;
     }
 }
