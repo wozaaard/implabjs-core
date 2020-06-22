9df76ae2b63d257c930f9546ed5ef15ff1ab044e
cin <90252519+wozaaard@users.noreply.github.com>
2020-06-22 09:59:48 +0300

    working on di decorators

--- src/main/ts/di/Annotations.ts
+++ src/main/ts/di/Annotations.ts
@@ -10,28 +10,41 @@ type Injector<T> = {
     [k in keyof T]: Setter;
 };
 
+type Compatible<T1, T2> = T1 extends T2 ? any : never;
+
+type SetterType<T> = T extends (v: infer V) => void ? V : never;
+
+type Tuple<T = any> = Parameters<(...args: T[]) => void>;
+
+interface Newable<A extends Tuple, T> {
+    new (...params: A): T;
+    prototype: T;
+}
+
+type MapTuple<T, A extends (keyof T)[]> = { [K in keyof A] : K extends number ? T[ A[K] ] : A[K] };
+
 export class Builder<T, S> {
-    provides(name: string) {
+    provides() {
         return <C extends Constructor<T>>(constructor: C) => {
             return constructor;
         };
     }
 
     inject<K extends keyof S>(dependency: K) {
-        return <M extends keyof T>(target: any, memberName: M, descriptor: TypedPropertyDescriptor<Setter<S[K]>> ) => {
+        // K = "bar"
+        // M = "setValue"
+        // S[K] = Bar
+        // T[M] = (value: string) => void
+        // P[m] = (value: V) => void
+        return <P, M extends keyof (T | P)>(target: P, memberName: M, descriptor: TypedPropertyDescriptor<Compatible<T[M], Setter<S[K]>>>) => {
 
         };
     }
 
-    prop<K extends keyof S>(dependency: K) {
-        return <M extends keyof T>(target: any, memberName: M, descriptor: TypedPropertyDescriptor<S[K]> ) => {
-
-        };
+    dependencies<D extends (keyof S)[]>(...deps: D) {
+        return <C extends Constructor<T>>(constructor: MapTuple<S, D> extends ConstructorParameters<C> ? C : never) => {
+            return constructor;
+        } ;
     }
-}
 
-export function inject<I extends Injector<I> >(dependency: string) {
-    return <M extends keyof I>(target: any, memberName: M, descriptor: TypedPropertyDescriptor<I[M]> ) => {
-
-    };
 }
